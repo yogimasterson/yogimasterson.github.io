3eff2332e37f8093a25b8c5b36bf06af5f2ce7a3
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-12-15 13:11:14 -0800

    edit port to be used with openshift

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const routes = require('./routes')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+// const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,8 +18,12 @@ app.use(bodyParser.json())
 
 app.use('/', routes)
 
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+// app.listen(PORT, () => {
+//   console.log(`Listening on http://localhost:${PORT}`)
+// })
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 module.exports = app
